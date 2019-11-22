94355d99409a0e3a4c4ccd91596db5c621f4f52c
centrevillage <centrevillage@gmail.com>
2019-11-23 00:52:15 +0900

    add validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,5 +3,6 @@ class Company < ApplicationRecord
   has_many :staffs, dependent: :nullify
 
   validates :uid, :name, presence: true, length: { maximum: 50 }
+  validates :uid, uniqueness: true
 end
 
