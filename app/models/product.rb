4498907c8edaca0e1f6bbdf2eefeae8c9e247ef6
centrevillage <centrevillage@gmail.com>
2019-12-02 00:04:03 +0900

    サブカテゴリ入力追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,5 +8,13 @@ class Product < ApplicationRecord
   validates :product_code, :title, allow_blank: true, length: { maximum: 50 }
   validates :video_url, allow_blank: true, length: { maximum: 256 }
   validates :qualification_comment, :description_a, :description_b, :video_comment, :video_license_memo, allow_blank: true, length: { maximum: 500 }
+
+  accepts_nested_attributes_for :product_sub_categories, reject_if: :all_blank, allow_destroy: true
+
+  class << self
+    def form_attribute_names
+      super + [{product_sub_categories_attributes: [:id, :sub_category_id, :_destroy]}]
+    end
+  end
 end
 
