58661615bcfa82c04bbcc352e61d291a6ada05d2
centrevillage <centrevillage@gmail.com>
2019-12-09 18:42:11 +0900

    レイアウト調整、associationのcollection指定

--- db/fixtures/staging/04_staff.rb
+++ db/fixtures/staging/04_staff.rb
@@ -1,7 +1,17 @@
 # coding: utf-8
 
-if user = Staff.find_or_initialize_by(login_name: 'TESTKENK/staff')
+if user = Staff.find_or_initialize_by(login_name: 'staff1')
   user.company = Company.find_by(name: 'テスト建機1')
+  user.staff_role = 'write'
   user.password = 'kenki012'
+  user.name = '入力ユーザー1'
+  user.save!
+end
+
+if user = Staff.find_or_initialize_by(login_name: 'staff2')
+  user.company = Company.find_by(name: 'テスト建機1')
+  user.staff_role = 'read'
+  user.password = 'kenki012'
+  user.name = '照会ユーザー1'
   user.save!
 end
