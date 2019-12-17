ce58830c6af60ef720ae380a194e73d3e739b7e0
centrevillage <centrevillage@gmail.com>
2019-12-17 13:28:00 +0900

    ログイン後の遷移先ページを設定

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,5 +2,11 @@ class TestsController < ApplicationController
   skip_authorization_check
 
   def test
+    case current_user
+    when Administrator
+      redirect_to companies_url
+    when Staff
+      redirect_to products_url
+    end
   end
 end
