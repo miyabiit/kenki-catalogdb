11afd3aa60465b183e4157d69a560bb6fdb8c68d
centrevillage <centrevillage@gmail.com>
2020-01-07 11:40:31 +0900

    自社在庫に属性の紐付けを可能に

--- app/models/stock_product.rb
+++ app/models/stock_product.rb
@@ -10,9 +10,9 @@ class StockProduct < ApplicationRecord
   has_many :sub_categories, through: :stock_product_sub_categories 
 
   has_many :stock_product_stored_props, dependent: :destroy
-  has_many :stock_product_text_props, -> { where(stored_prop_type: 'TextProp') }
-  has_many :stock_product_file_props, -> { where(stored_prop_type: 'FileProp') }
-  has_many :stock_product_image_props, -> { where(stored_prop_type: 'ImageProp') }
+  has_many :stock_product_text_props, -> { where(stored_prop_type: 'TextProp') }, class_name: 'StockProductStoredProp'
+  has_many :stock_product_file_props, -> { where(stored_prop_type: 'FileProp') }, class_name: 'StockProductStoredProp'
+  has_many :stock_product_image_props, -> { where(stored_prop_type: 'ImageProp') }, class_name: 'StockProductStoredProp'
 
   accepts_nested_attributes_for :stock_product_sub_categories, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :stock_product_text_props, allow_destroy: true, reject_if: :all_blank
