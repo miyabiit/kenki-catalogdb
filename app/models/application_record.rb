8d3a440a9b2f18ce2f1b17e326e9b821119b6e46
centrevillage <centrevillage@gmail.com>
2020-03-09 20:53:20 +0900

    issue #6 検索用オペレータ($or $like $lt $gt $lte $gte $in)の追加

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -30,21 +30,54 @@ class ApplicationRecord < ActiveRecord::Base
 
     def search(params)
       params ||= {}
-      q = all
+      parse_search_param(all, params, nil)
+    end
+
+    def parse_search_param(q, params, context_key)
+      context_table_name = self.table_name
       params = params.select do |key, value|
-        if value.is_a? Hash
-          if value.present?
-            ref_klass = key.singularize.camelize.constantize
-            ref_q = ref_klass.search(value).where("#{ref_klass.table_name}.#{self.table_name.singularize}_id = #{self.table_name}.id")
-            q = q.where("EXISTS(#{ref_q.to_sql})")
+        if key[0] == '$'
+          case key
+          when '$or'
+            next unless value.is_a? Array
+            value.each do |v|
+              q = q.or(parse_search_param(q, v, context_key))
+            end
+          when '$like'
+            next unless value.is_a? String
+            q = q.where(["#{context_table_name}.#{context_key} LIKE ?", "%#{value}%"])
+          when '$eq'
+            q = q.where(["#{context_table_name}.#{context_key} = ?", value])
+          when '$lt'
+            q = q.where(["#{context_table_name}.#{context_key} < ?", value])
+          when '$gt'
+            q = q.where(["#{context_table_name}.#{context_key} > ?", value])
+          when '$lte'
+            q = q.where(["#{context_table_name}.#{context_key} <= ?", value])
+          when '$gte'
+            q = q.where(["#{context_table_name}.#{context_key} >= ?", value])
+          when '$ne'
+            q = q.where(["#{context_table_name}.#{context_key} != ?", value])
+          when '$in'
+            q = q.where(["#{context_table_name}.#{context_key} IN (?)", value])
+          else
+            raise ArgumentError, "未対応の検索オペレーター - #{key}"
           end
-          false
+        elsif value.is_a? Hash
+          if value.present?
+            operator_hash, relation_attr_hash = value.partition {|k, v| k[0] == '$'}.map(&:to_h)
+            ref_klass = key.singularize.camelize.constantize
+            ref_q = ref_klass.search(relation_attr_hash).where("#{ref_klass.table_name}.#{context_table_name.singularize}_id = #{context_table_name}.id")
+            q = q.where("EXISTS(#{ref_q.to_sql})")
+            operator_hash.each do |k, v|
+              q = parse_search_param(q, v, key)
+            end
+          end
         elsif attribute_names.include?(key)
           if self.column_for_attribute(key).type == :string && value.present?
             if value.present?
-              q = q.where(["#{self.table_name}.#{key} LIKE ?", "%#{value}%"])
+              q = q.where(["#{context_table_name}.#{key} LIKE ?", "%#{value}%"])
             end
-            false
           else
             value.present?
           end
@@ -54,10 +87,10 @@ class ApplicationRecord < ActiveRecord::Base
             target_time = Time.parse(value)
             q = q.where(real_key => (target_time.beginning_of_day .. target_time.end_of_day))
           end
-          false
+        else
+          q = q.where(Hash[key, value])
         end
       end
-      q = q.where(params)
       q
     end
   end
