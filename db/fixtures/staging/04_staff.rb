836934f906f472c7c30771ed4110977be73bd119
centrevillage <centrevillage@gmail.com>
2020-03-17 11:36:11 +0900

    fix seed_fu fixture

--- db/fixtures/staging/04_staff.rb
+++ db/fixtures/staging/04_staff.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 if user = Staff.find_or_initialize_by(login_name: 'staff1')
-  user.company = Company.find_by(name: 'テスト建機1')
+  user.company = Company.find_by(uid: 'TESTKENK')
   user.staff_role = 'write'
   user.password = 'kenki012'
   user.name = '入力ユーザー1'
@@ -9,7 +9,7 @@ if user = Staff.find_or_initialize_by(login_name: 'staff1')
 end
 
 if user = Staff.find_or_initialize_by(login_name: 'staff2')
-  user.company = Company.find_by(name: 'テスト建機1')
+  user.company = Company.find_by(uid: 'TESTKENK')
   user.staff_role = 'read'
   user.password = 'kenki012'
   user.name = '照会ユーザー1'
