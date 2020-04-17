d8ec9ae3f17384c463816c0c6b8da341ee22868a
centrevillage <centrevillage@gmail.com>
2020-04-17 17:08:23 +0900

    属性が直接削除された場合は自社在庫からもリレーションを削除する様に

--- app/models/stored_prop.rb
+++ app/models/stored_prop.rb
@@ -4,6 +4,7 @@ class StoredProp < ApplicationRecord
   belongs_to :source, optional: true, class_name: 'StoredProp'
   belongs_to :company, optional: true
 
+  has_many :stock_product_stored_props, dependent: :destroy, inverse_of: :stored_prop
   has_many :stored_prop_sub_categories, dependent: :destroy
   has_many :sub_categories, through: :stored_prop_sub_categories
 
