49e3470c8aafa073904770e0192d9f22a4abc715
centrevillage <centrevillage@gmail.com>
2020-04-24 13:10:30 +0900

    issue #25 会社削除時に関連情報削除

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,9 @@
 class Company < ApplicationRecord
-  has_many :managers, dependent: :nullify
   has_many :staffs, dependent: :nullify
   has_many :sub_categories, dependent: :destroy
+  has_many :categories, dependent: :destroy
   has_many :stored_props, dependent: :destroy
+  has_many :stock_products, dependent: :destroy
 
   validates :uid, :name, presence: true, length: { maximum: 50 }
   validates :uid, uniqueness: { case_sensitive: true }
