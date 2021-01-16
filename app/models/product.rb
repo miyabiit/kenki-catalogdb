0525d45f097107382cee6ed0204d8f1c51562162
centrevillage <centrevillage@gmail.com>
2021-01-16 10:48:14 +0900

    issue #48 商品共通情報の説明A/Bの桁数を2500文字までに

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :product_code, :title, presence: true
   validates :product_code, :title, :product_name, :product_short_name, :product_model_name, :manufacture_name, allow_blank: true, length: { maximum: 50 }
-  validates :qualification_comment, :description_a, :description_b, allow_blank: true, length: { maximum: 500 }
+  validates :qualification_comment, :description_a, :description_b, allow_blank: true, length: { maximum: 2500 }
 end
 
